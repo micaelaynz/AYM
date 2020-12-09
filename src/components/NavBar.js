037e1bb552a617b777bec2c5fa5c0e1152942f60
micaelaynz <mica.yanez@hotmail.com>
2020-12-09 17:27:34 -0300

    menu ecommerce

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,17 +4,17 @@ const NavBar = () => {
 
     return(
    <>
-   <Navbar bg="light" variant="light">
-    <Navbar.Brand href="#home">AM</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#secciones">Secciones</Nav.Link>
-      <Nav.Link href="#marcas">Marcas</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Buscar un producto" className="mr-sm-2" />
-      <Button variant="outline-primary">Search</Button>
-    </Form>
-    </Navbar>
+   <navbar bg="light" variant="light">
+    <navbar.brand href="#home">A&M</navbar.brand>
+    <nav className="mr-auto">
+      <nav.Link href="#secciones">Secciones</nav.Link>
+      <nav.Link href="#marcas">Marcas</nav.Link>
+    </nav>
+    <form inline>
+      <formControl type="text" placeholder="Buscar un producto" className="mr-sm-2" />
+      <button variant="outline-primary">Search</button>
+    </form>
+    </navbar>
    </>
   )
  }
